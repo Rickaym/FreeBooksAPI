d96fc20f49d878ba345631b8884cbbfc35af1f79
Ricky <psmtitant@gmail.com>
2022-10-22 22:05:31 +0630

    refactor: remove `last-added` added `datadumps`
    Last ended queries should go through the normal search endpoint with
    a mode specifier.

--- freebooksapi/main.py
+++ freebooksapi/main.py
@@ -1,15 +1,15 @@
 import logging
-from logging import getLogger
-from typing import Dict, Optional
+
 from fastapi import FastAPI
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from models import MetaPublicationModel
+from logging import getLogger
+from typing import Dict, List, Optional
+from misc import cache_cascade, set_cache
+from exceptions import ErrorJsonResponse
+from models import MetaPublicationModel, LibraryAll
 from scrapers.agent import Agent
 from scrapers.genlibrusec import GenLibRusEc
 from scrapers.libgenlc import LibGenLc
-from scrapers.objects import SearchUrlArgs
-from models import LibraryAll
+from scrapers.objects import SearchUrlArgs, SearchMode
 
 FREEBOOKSAPI = FastAPI()
 
@@ -37,44 +37,28 @@ CURRENT_VERSION = "v1"
 VERSIONS = [CURRENT_VERSION]
 
 
-class ErrorJsonResponse(JSONResponse):
-    def __init__(self, status_code: int, status_name: str, message: str) -> None:
-        super().__init__(
-            jsonable_encoder(
-                {
-                    "error": {
-                        "code": status_code,
-                        "message": message,
-                        "status": status_name,
-                    }
-                }
-            ),
-            status_code=status_code,
-        )
+@FREEBOOKSAPI.get("/{library}/datadumps", response_model=Dict[str, str])
+async def get_datadumps(library: LibraryAll):
+    return await LIBRARY_AGENTS[library.value].get_datadumps()
 
 
-@FREEBOOKSAPI.on_event("startup")
-async def startup_event():
-    pass
+async def cache_get_enlisted_topics(cache_id: str):
+    log.info(f'Retrieving cache information under "{cache_id}"')
+    for name, lib in LIBRARY_AGENTS.items():
+        set_cache(cache_id.format(library=name), await lib.get_topics())
 
 
-@FREEBOOKSAPI.get(
-    "/{library}/topics",
+@FREEBOOKSAPI.get("/{library}/topics", response_model=Dict[str, str])
+@cache_cascade(
+    cache_id="{library}/topics",
+    cache_every_h=24,
+    release_after_h=48,
+    caching_task=cache_get_enlisted_topics,
 )
 async def get_enlisted_topics(library: LibraryAll):
     """
     Retrieve available topics for the library. The topic IDs fetched here can
     be used in the `/search` endpoint via `topic_id`.
-    """
-    return await LIBRARY_AGENTS[library].get_topics()
-
-
-@FREEBOOKSAPI.get(
-    "/{library}/last-added",
-)
-def get_last_added(library: LibraryAll):
-    """
-    Retrieve most recently added publications.
     """
     pass
 
@@ -82,15 +66,16 @@ def get_last_added(library: LibraryAll):
 @FREEBOOKSAPI.get(
     "/{library}/aliases",
     response_description="Library URL Aliases.",
+    response_model=List[str],
 )
 def get_aliases(library: LibraryAll):
     """
     Retrieve existing aliases for the given libraries.
     """
-    if (library.value == LibraryAll.libgen.value):
+    if library is LibraryAll.libgen:
       return ["http://libgen.rs/", "http://libgen.is/", "http://libgen.st/"]
-    elif (library.value == LibraryAll.libgenlc.value):
-      return ['http://libgen.lc/', 'http://libgen.gs/', 'http://libgen.li/']
+    elif library is LibraryAll.libgenlc:
+        return ["http://libgen.lc/", "http://libgen.gs/", "http://libgen.li/"]
 
 
 @FREEBOOKSAPI.get(
@@ -98,31 +83,39 @@ def get_aliases(library: LibraryAll):
     response_description="A list of publications for the given query.",
     response_model=MetaPublicationModel,
 )
-def get_publications(
+async def get_publications(
     library: LibraryAll,
-    query: str,
+    query: Optional[str] = None,
     topic_id: Optional[int] = None,
     limit: int = 25,
     offset: int = 0,
     lang: Optional[str] = None,
     page: int = 1,
+    search_mode: Optional[SearchMode] = None,
 ):
     """
     Retrieve all publications under a query.
 
-    To get all publications under a specific topic, you can specify the query
-    as `'*'` with a valid topic id.
+    To get all publications under a specific topic, you can specify a valid topic id.
+    To get all latest publications, specify search_mode as last.
     """
-    if query == "*" and topic_id is None:
+    if query is None and (topic_id is None or search_mode is None):
         return ErrorJsonResponse(
-            404, "BADARGUMENT", f"You cannot use the '*' query without a topic id."
+            404,
+            "BADARGUMENT",
+            f"You cannot use leave the query empty without a topic id or a search mode.",
         )
 
     agent = LIBRARY_AGENTS[library.value]
-    result = agent.query(
+    result = await agent.query(
         query,
         SearchUrlArgs(
-            lang=lang, page=page, topic_id=topic_id, limit=limit, offset=offset
+            lang=lang,
+            page=page,
+            topic_id=topic_id,
+            limit=limit,
+            offset=offset,
+            search_mode=search_mode,
         ),
     )
     if not result:
