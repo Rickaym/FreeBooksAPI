bc69fa7c0e87d2f8008224da50c890f0c7f6b606
Ricky <66440980+Rickaym@users.noreply.github.com>
2022-11-13 00:29:38 +0630

    delete: remove zlibrary enum

--- freebooksapi/models.py
+++ freebooksapi/models.py
@@ -1,11 +1,11 @@
 from enum import Enum
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
 
 from pydantic import BaseModel
 
 
-class LibraryZlib(str, Enum):
-    zlibrary = "zlibrary"
+class LibraryPlanetEBooks(str, Enum):
+    planetebooks = "planetebooks"
 
 
 class LibraryLibgen(str, Enum):
@@ -16,7 +16,7 @@ class LibraryLibgen(str, Enum):
 class LibraryAll(str, Enum):
     libgen = LibraryLibgen.libgen.value
     libgenlc = LibraryLibgen.libgenlc.value
-    zlibrary = LibraryZlib.zlibrary.value
+    planetebooks = LibraryPlanetEBooks.planetebooks.value
 
 
 class DatadumpModel(BaseModel):
@@ -24,7 +24,9 @@ class DatadumpModel(BaseModel):
     url = "https://libgen.is/dbdumps/fiction.rar"
     last_modified: str = "2022-10-24 04:37"
     size: str = "1.0G"
-    description: str = "Housebound dogs: how to keep your stay-at-home dog happy & healthy"
+    description: str = (
+        "Housebound dogs: how to keep your stay-at-home dog happy & healthy"
+    )
 
 
 class MetaDatadumpModel(BaseModel):
@@ -34,24 +36,24 @@ class MetaDatadumpModel(BaseModel):
 
 
 class PublicationModel(BaseModel):
-    id = 1016852
-    authors = "Paula Kephart"
+    id: Union[str, int] = 1016852
+    authors: Optional[str] = "Paula Kephart"
     isbn: Optional[List[str]] = ["1580173624", "9781580173629"]
     edition: Optional[str] = None
     series: Optional[str] = "A Storey country wisdom bulletin, A-270"
     title = "Housebound dogs: how to keep your stay-at-home dog happy & healthy"
     publisher: Optional[str] = "Storey Publishing, LLC"
-    year = 2000
+    year: Optional[Union[int, str]] = 2000
     pages = "32"
     lang = "English"
     size = "2 Mb"
     extension = "epub"
-    mirrors: Dict[str, str] = {
-        "libgen.io": "http://library.lol/main/708C30851BCF1D551335479CEEB1B90F",
-        "libgen.pw": "http://libgen.lc/ads.php?md5=708C30851BCF1D551335479CEEB1B90F",
-        "b-ok.org": "https://3lib.net/md5/708C30851BCF1D551335479CEEB1B90F",
-        "bookfi.net": "https://library.bz/main/edit/708C30851BCF1D551335479CEEB1B90F",
-    }
+    mirrors: List[str] = [
+        "http://library.lol/main/708C30851BCF1D551335479CEEB1B90F",
+        "http://libgen.lc/ads.php?md5=708C30851BCF1D551335479CEEB1B90F",
+        "https://3lib.net/md5/708C30851BCF1D551335479CEEB1B90F",
+        "https://library.bz/main/edit/708C30851BCF1D551335479CEEB1B90F",
+    ]
 
 
 class MetaPublicationModel(BaseModel):
