be5921909d0ce364d669c298eb8552892b43fe44
Ricky <66440980+Rickaym@users.noreply.github.com>
2023-01-04 02:22:54 +0630

    api reference link fix

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -44,7 +44,7 @@ const sidebars = {
     {
       type: "link",
       label: "API Reference",
-      href: "https://freebooksapi.pyaesonemyo.me/latest/docs/",
+      href: "https://freebooksapi.pyaesonemyo.me/api/latest/docs/",
     },
   ],
 };
