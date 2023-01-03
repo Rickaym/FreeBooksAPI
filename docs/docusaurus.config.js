cb73d880a2b6c1d744378f81ed2c53d158395b14
Ricky <66440980+Rickaym@users.noreply.github.com>
2023-01-04 01:37:02 +0630

    fix edit this page url

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Rickaym/FreeBooksAPI/tree/master/docs/docs',
+            'https://github.com/Rickaym/FreeBooksAPI/tree/master/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
